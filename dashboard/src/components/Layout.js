d092bf53095061c52cd7ea489eedc0f3066bd8ac
Anish <anish@codeanish.com>
2020-11-12 20:37:39 +0000

    Adding some basic styles to twitter card

--- dashboard/src/components/Layout.js
+++ dashboard/src/components/Layout.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './layout.module.css'
 
 const Layout = ({children}) => {
     return(
-        <div>
+        <div className={styles.wrapper}>
             {children}
         </div>
     )
--- dashboard/src/components/TwitterFollowersCard.js
+++ dashboard/src/components/TwitterFollowersCard.js
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+
 
 const TwitterFollowersCard = () => {
 
     const [followers, setFollowers] = useState();
 
+    const StyledCard = styled.div`
+    background-image: linear-gradient(to top right, #3020D3, #1F1596); 
+    color: white;
+    width: 200px;
+    height: 100px;
+    border-radius: 5px;
+`
+
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/twitterfollowers?as_of_date=2020-11-10', 
+        fetch('http://127.0.0.1:5000/twitterfollowers?as_of_date=2020-11-12', 
         {
             method: "GET"
         })
@@ -13,9 +24,30 @@ const TwitterFollowersCard = () => {
         .then(response => {setFollowers(response.followers)})
     })
 
+    var containerStyle={
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '10px',                
+        textAlign: 'center'
+    }
+
+    var countStyle={
+        fontSize: '48px'        
+    }
+
+    var socialStyle={
+        fontSize: '20px'
+    }
+
+
     return(
         <div>
-            {followers}
+            <StyledCard>
+                <div style={containerStyle}>
+                    <div style={socialStyle}>Twitter</div>                    
+                    <div style={countStyle}>{followers}</div>
+                </div>
+            </StyledCard>            
         </div>
     )
 }
